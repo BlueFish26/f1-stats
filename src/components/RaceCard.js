925ae3ed3e4b7479f07e0fb3b5dd2e67591772be
Norman <bluefish102615@gmail.com>
2021-04-29 19:48:45 +0800

    Added flags

--- src/components/RaceCard.js
+++ src/components/RaceCard.js
@@ -9,7 +9,22 @@ const RaceCard = ({ race }) => {
         <p>
           <b>{race.Circuit.Location.country}</b>
         </p>
-        <p>{race.Circuit.circuitName}</p>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <img
+            src={`../imgs/flags/${race.Circuit.Location.country}.png`}
+            style={{
+              width: "15%",
+              height: "2rem",
+              border: "1px solid grey",
+              borderRadius: "1px",
+              boxShadow: "1px 1px grey",
+              marginRight: "0.5rem",
+            }}
+          />
+          <div>
+            <h5>{race.Circuit.circuitName}</h5>
+          </div>
+        </div>
       </Card.Content>
     </Card>
   );
