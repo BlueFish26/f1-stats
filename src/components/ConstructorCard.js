a5f30069be0d0a15ff1440a9cb8c0ad7eb78c38c
Norman <bluefish102615@gmail.com>
2021-05-03 15:08:36 +0800

    Improved look and feel of constructor card, added helmet images

--- src/components/ConstructorCard.js
+++ src/components/ConstructorCard.js
@@ -1,4 +1,4 @@
-import './constructor.css';
+import "./constructor.css";
 
 const ConstructorCard = ({ constructor }) => {
   return (
@@ -25,37 +25,37 @@ const ConstructorCard = ({ constructor }) => {
               />
             </div>
           </div>
-          <div className="drivers">
-            <div className="driver">
-              <div className={`name ${constructor.Constructor.constructorId}`}>
-                {constructor.drivers[0].givenName}{' '}
-                {constructor.drivers[0].familyName}
-              </div>
-              <div className="image">
-                <img
-                  src={`imgs/drivers/${constructor.drivers[0].driverId}.png`}
-                  alt=""
-                />
-              </div>
-            </div>
-            <div className="driver">
-              <div className={`name ${constructor.Constructor.constructorId}`}>
-                {constructor.drivers[1].givenName}{' '}
-                {constructor.drivers[1].familyName}
-              </div>
-              <div className="image">
-                <img
-                  src={`imgs/drivers/${constructor.drivers[1].driverId}.png`}
-                  alt=""
-                />
-              </div>
-            </div>
-          </div>
           <div className="car-image">
             <img
               src={`imgs/constructors/${constructor.Constructor.constructorId}_car.png`}
               alt=""
             />
+          </div>
+          <div className="drivers">
+            <div className="driver">
+              <div className={`name ${constructor.Constructor.constructorId}`}>
+                {constructor.drivers[0].givenName}{" "}
+                {constructor.drivers[0].familyName}
+              </div>
+              <div className="image">
+                <img
+                  src={`imgs/drivers/helmets/${constructor.drivers[0].driverId}.png`}
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="driver">
+              <div className={`name ${constructor.Constructor.constructorId}`}>
+                {constructor.drivers[1].givenName}{" "}
+                {constructor.drivers[1].familyName}
+              </div>
+              <div className="image">
+                <img
+                  src={`imgs/drivers/helmets/${constructor.drivers[1].driverId}.png`}
+                  alt=""
+                />
+              </div>
+            </div>
           </div>
         </div>
       </fieldset>
--- src/views/Teams.js
+++ src/views/Teams.js
@@ -1,8 +1,7 @@
-import React, { Fragment, useEffect } from 'react';
-import Constructor from '../components/Constructor';
-import ConstructorCard from '../components/ConstructorCard';
-import { useF1Context } from '../contexts/F1Context';
-import { getConstructors } from '../reducers/f1Actions';
+import { Fragment, useEffect } from "react";
+import ConstructorCard from "../components/ConstructorCard";
+import { useF1Context } from "../contexts/F1Context";
+import { getConstructors } from "../reducers/f1Actions";
 
 const Teams = () => {
   const [{ constructors }, dispatch] = useF1Context();
@@ -11,7 +10,7 @@ const Teams = () => {
     async function loadConstructors() {
       await getConstructors(dispatch);
     }
-    console.log('Loading Constructors...');
+    console.log("Loading Constructors...");
     loadConstructors();
   }, [dispatch]);
 
